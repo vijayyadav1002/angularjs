80b8e818ebd897c90ae7646a053326cbbf040bb4
Vijay Yadav <Vijay.Yadav@talentica.com>
2015-12-30 19:38:09 -0500

    directive talking to controller

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var app = angular.module('behaviour', []);
 
-app.directive('enter', [function(){
+app.controller('TwitterCtrl', ["$scope", function($scope){
+  
+  $scope.fireMe = function() {
+    console.log("Firing AK");
+    console.log($scope.message);
+  }
+}]);
 
+app.directive('enter', [function(){
   return {
     restrict: 'A',
     link: function($scope, iElm, attr) {
         iElm.bind("mouseenter", function () {
-          console.log("I am entering into");
-            iElm.addClass(attr.enter);
+          $scope.$apply(attr.enter);
         });
     }
   };
