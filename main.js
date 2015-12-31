f839c4d8e565e0e164acd546300c5c8c7a023b35
Vijay Yadav <Vijay.Yadav@talentica.com>
2015-12-30 19:24:47 -0500

    created directive

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function FirstCtrl ($scope) {
-  $scope.data = { message :"Hello Controller"};
-}
+var app = angular.module('behaviour', []);
+
+app.directive('enter', [function(){
+
+  return {
+    restrict: 'A',
+    link: function($scope, iElm, attr) {
+        iElm.bind("mouseenter", function () {
+          console.log("I am entering into");
+            iElm.addClass(attr.enter);
+        });
+    }
+  };
+}]);
