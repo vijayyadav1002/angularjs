b23ef49353b9b6e63d98499a6f830bad902b997e
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-01 00:36:46 -0500

    directive talking directive

--- main.js
+++ main.js
@@ -1,20 +1,63 @@
-var app = angular.module('behaviour', []);
+var app = angular.module('superheroapp', []);
 
-app.controller('TwitterCtrl', ["$scope", function($scope){
-  
-  $scope.fireMe = function() {
-    console.log("Firing AK");
-    console.log($scope.message);
-  }
-}]);
-
-app.directive('enter', [function(){
+app.directive('superhero', [function(){
+  // Runs during compile
   return {
-    restrict: 'A',
-    link: function($scope, iElm, attr) {
-        iElm.bind("mouseenter", function () {
-          $scope.$apply(attr.enter);
-        });
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    scope: {}, //defined empty to isolate the scope from other directives
+    controller: ['$scope', function($scope) {
+      $scope.powers = [];
+      this.addSpeed = function () {
+        $scope.powers.push('speed');
+      };
+      this.addFlight = function () {
+        $scope.powers.push('flight');
+      };
+      this.addStrength = function () {
+        $scope.powers.push('strength');
+      };
+      $scope.showItems = function () {
+        console.log($scope.powers);
+      }
+    }],
+    link: function($scope, iElm, iAttrs, controller) {
+      console.log($scope.powers);
+      iElm.bind('click', function(){
+        $scope.showItems();
+      });
     }
   };
 }]);
+
+app.directive('strength', function(){
+  // Runs during compile
+  return {
+    restrict: 'A',
+    require: 'superhero', // Array = multiple requires, ? = optional, ^ = check parent elements
+    link: function($scope, iElm, iAttrs, controller) {
+      controller.addStrength();
+    }
+  };
+});
+
+app.directive('flight', function(){
+  // Runs during compile
+  return {
+    restrict: 'A',
+    require: 'superhero', // Array = multiple requires, ? = optional, ^ = check parent elements
+    link: function($scope, iElm, iAttrs, controller) {
+      controller.addFlight();
+    }
+  };
+});
+
+app.directive('speed', function(){
+  // Runs during compile
+  return {
+    restrict: 'A',
+    require: 'superhero', // Array = multiple requires, ? = optional, ^ = check parent elements
+    link: function($scope, iElm, iAttrs, controller) {
+      controller.addSpeed();
+    }
+  };
+});
