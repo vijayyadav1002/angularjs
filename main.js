0f5108e86ffd9f951ba2ca557ef85ca60fe2455c
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-03 11:24:03 -0500

    isolate scope '&' talking directive to controller

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-var app = angular.module('drinkApp', []);
+var app = angular.module('phoneApp', []);
 
-app.controller('drinkCtrl', ['$scope', function($scope){
-}]);
-
-app.directive('drink', [function(){
-  return {
-    scope: {
-      flavour: '=', //this = represent that flavour attribute in the directive is a binds to another model and they both are two way binded
-    },
-    template: '<input type="text" ng-model="flavour"/><div>{{flavour}}</div>'
+app.controller('phoneCtrl', ['$scope', function($scope){
+  $scope.callHome = function(message) {
+    console.log(message);
   };
 }]);
+
+app.directive('phone', [function(){
+  return {
+    scope: {
+      dial: '&',
+    },
+    template: '<input type="text" ng-model="value"/>' + 
+    '<div class="button" ng-click="dial({message: value})">Call home</div>'
+  };
+}]);
+
+/**
+ *  Note: The message property should have the same name as passed from the html callHome function
+ * to get passed.
+ */
