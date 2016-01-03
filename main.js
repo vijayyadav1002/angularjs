6c74205b9d009323c6de7920470a159aed464664
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-03 11:09:49 -0500

    isolate scope using "="

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ app.controller('drinkCtrl', ['$scope', function($scope){
 app.directive('drink', [function(){
   return {
     scope: {
-      flavour: '@', //this @ represent that flavour attribute in the directive is a string
+      flavour: '=', //this = represent that flavour attribute in the directive is a binds to another model and they both are two way binded
     },
-    template: '<div>{{flavour}}</div>'
+    template: '<input type="text" ng-model="flavour"/><div>{{flavour}}</div>'
   };
 }]);
