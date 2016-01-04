b0662628f63b5213394d41f2478869d835dd6738
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-04 00:06:46 -0500

    angular element  * 1. camelCased directive are written in html with hyphen separated  * 2. if compile is used than link function is manually triggered  * 3. Element can be referenced by creating angular element first  * this way you don't have to find the element and using the big expression

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 var app = angular.module('phoneApp', []);
 
-app.controller('phoneCtrl', ['$scope', function($scope){
-}]);
-
-app.directive('zippy', [function(){
-  // Runs during compile
-  return {
-    scope: {
-      title: '@'
-    }, // {} = isolate, true = child, false/undefined = no change
-    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-    template: '<div class="pannel">' +
-    '<div ng-click="toggleContent()">{{title}}</div>' + 
-    '<div ng-show="isContentVisible" ng-transclude></div>' +
-    '</div>',
-    transclude: true,
-    link: function($scope) {
-      $scope.isContentVisible = false;
-      $scope.toggleContent = function() {
-        $scope.isContentVisible = !$scope.isContentVisible;
+app.directive('dummyPassword', [function(){
+  var validElement = angular.element("<div>{{title}}</div>");
+  var link = function(scope) {
+    scope.$watch("title", function(value) {
+      if (value == 'password') {
+        validElement.toggleClass('alert-box alert');
       };
+    });
+  };
+  return {
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    replace: true, //this will replace the directive name with the actual html 
+    template: '<div class="pannel"><input type="text" ng-model="title"/></div>',
+    compile: function(tElem) {
+      tElem.append(validElement);
+      return link;
     }
   };
 }]);
+
+/**
+ * note:
+ * 1. camelCased directive are written in html with hyphen separated
+ * 2. if compile is used than link function is manually triggered
+ * 3. Element can be referenced by creating angular element first
+ * this way you don't have to find the element and using the big expression 
+ */
 
