070f8178f7219010754c6dc0a45ad50be11eddfa
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-03 23:30:11 -0500

    building zippy using transclude directive and link function

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 var app = angular.module('phoneApp', []);
 
 app.controller('phoneCtrl', ['$scope', function($scope){
-  this.clickMe = function() {
-    console.log("how r u doing");
-  }
-  return $scope.phoneCtrl = this;
+}]);
+
+app.directive('zippy', [function(){
+  // Runs during compile
+  return {
+    scope: {
+      title: '@'
+    }, // {} = isolate, true = child, false/undefined = no change
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    template: '<div class="pannel">' +
+    '<div ng-click="toggleContent()">{{title}}</div>' + 
+    '<div ng-show="isContentVisible" ng-transclude></div>' +
+    '</div>',
+    transclude: true,
+    link: function($scope) {
+      $scope.isContentVisible = false;
+      $scope.toggleContent = function() {
+        $scope.isContentVisible = !$scope.isContentVisible;
+      };
+    }
+  };
 }]);
 
