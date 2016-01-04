cbb1ce39b44c913452afc1a234144c7741dd21e7
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-03 22:54:32 -0500

    transclude basics it is just to include the other component as shown in directive

--- main.js
+++ main.js
@@ -1,28 +1,12 @@
 var app = angular.module('phoneApp', []);
 
 app.controller('phoneCtrl', ['$scope', function($scope){
-  $scope.leaveVoiceMessage = function(number, message) {
-    console.log("calling at " + number + ":" +message + " |->network:" + $scope.network);
-  };
 }]);
 
 app.directive('phone', [function(){
   return {
     restrict: 'E',
-    scope: {
-      number: '@',
-      network: "=",
-      makeCall: '&'
-    },
-    template: '<div class="panel">' + 
-      '<div>Numbe is {{number}}</div>' + 
-      '<select ng-model="network"><option ng-repeat="nt in networks">{{nt}}</option></select>' +
-      '<input type="text" ng-model="message"/>' +
-      '<div class="button" ng-click="makeCall({number:number, message:message})">Call Home</div>'
-    ,
-    link: function(scope) {
-      scope.networks = ['Airtel', 'IDEA', 'Vodafone'];
-      scope.network = scope.networks[0];
-    }
+    transclude: true,
+    template: '<div class="panel" ng-transclude></div>'
   };
 }]);
