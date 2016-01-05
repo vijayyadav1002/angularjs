fea63d0cd884997ebf307837e87036c85b6facd6
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-05 13:30:43 -0500

    $index, $event and $log

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', []);
 
-app.factory("game", function() {
-	return {
-		game: "I am game"
-	}
-});
-
-angular.injector(['app']).invoke(['game', function(game) {
-	console.log("injector", game);
+app.config(['$logProvider', function(logProvider) {
+	logProvider.debugEnabled(true);//pass false to disbale debug logs
 }]);
 
-
-
-var viewCtrl = app.controller('AppCtrl', ['$scope', '$injector', function($scope, injector){
-	injector.invoke(['game', function(game){
-		  $scope.model = {
-        message: game.game
-      }
-	}]);
+app.run(['$rootScope', '$log', function(rootScope, log) {
+	rootScope.$log = log;
 }]);
-
-/**
-	* Note:
-	* only one injector instance per application and not per module
-	*/
-
-
