52463993d8f8acb226a23f9bf8c8afb9417aa4dd
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-05 10:52:43 -0500

    Directive for route handling error

--- main.js
+++ main.js
@@ -17,6 +17,20 @@ app.controller('AppCtrl', ['$rootScope', function(rootScope){
 	});
 }]);
 
+app.directive('error', ['$rootScope', function(rootScope) {
+	return {
+		restrict: 'E',
+		template: '<div class="alert-box alert" ng-show="isError">Error loading!!</div>',
+		link: function(scope) {
+			rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+				scope.isError = true;
+			});
+		}
+	}
+
+}]);
+
+
 var viewCtrl = app.controller('ViewCtrl', ['$scope', '$route',  function($scope, $route){
 	console.log($route);
   $scope.model = {
