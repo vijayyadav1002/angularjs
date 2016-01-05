59fe36d7de512e70fab0be8e67de703e08335956
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-05 11:43:51 -0500

    provider various use cases

--- main.js
+++ main.js
@@ -1,16 +1,55 @@
 var app = angular.module('app', []);
 
+app.provider("superhero", function() {
+	return {
+		$get: function() {
+			return {
+				superhero: "Shaktiman"
+			}
+		}
+	}
+});
+
+
 app.factory("game", function() {
 	return {
 		game: "I am game"
 	}
-})
+});
+
+app.provider("play", function() {
+	var type;
+	return {
+		setType: function(value) {
+			type = value;
+		},
+		$get: function() {
+			return {
+				title: type + "Craft"
+			}
+		}
+	}
+});
+
+app.config(['$provide', 'playProvider', function(provide, playProvider) {
+	provide.factory("gem", function() {
+		return {
+			gem: 'Ruby'
+		}
+	});
+
+	playProvider.setType("War");
+}]);
 
 
-var viewCtrl = app.controller('AppCtrl', ['$scope', 'game',  function($scope, game){
+
+var viewCtrl = app.controller('AppCtrl', ['$scope', 'game', 'gem', 'superhero', 'play',
+ function($scope, game, gem, superhero, play){
 	console.log(game);
+	console.log(gem);
+	console.log(superhero);
   $scope.model = {
-    message: "This is awesome app!! "
+    message: play.title
   }
 }]);
 
