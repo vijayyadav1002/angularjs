ff23edca66b279c114f3760d4bf775764ac513b5
Vijay Yadav <Vijay.Yadav@talentica.com>
2016-01-05 00:58:11 -0500

    use of redirectTo in angular route using config

--- main.js
+++ main.js
@@ -2,16 +2,30 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .when('/map/:country/:state/:city', {
+  .when('/app', {
     templateUrl: "app.html",
     controller: "AppCtrl"
+  })
+  .when('/pizza/:crust/:topping', {
+    redirectTo: function(routeParams, path, search) {
+			console.log(routeParams);
+			console.log(path);
+			console.log(search);
+			return "/" + routeParams.crust;
+    }
+  })
+  .when('/deep', {
+  	template: 'deep dish'
+  })
+  .otherwise({
+  	redirectTo: '/app'
   });
+
 }]);
 
 app.controller('AppCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
   $scope.model = {
-    message: "Address: " + $routeParams.country +
-    ", " + $routeParams.state + ", " + $routeParams.city
+    message: "This is awesome app!! "
   }
 }]);
 
